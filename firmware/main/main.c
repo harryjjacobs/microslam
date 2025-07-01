3ca663964cf6ec7cc234f55e15ff7a63fd80e93a
harryjjacobs <harryjjacobs@googlemail.com>
2025-07-01 16:58:58 +0100

    initialise slam system in program entrypoint

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -1,52 +1,39 @@
-/* Blink Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 
-#include "driver/gpio.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
-#include "led_strip.h"
 #include "sdkconfig.h"
 
-static const char *TAG = "microslam";
+// microslam includes
+#include "logging.h"
+#include "platform.h"
+#include "state.h"
+#include "system.h"
 
-// For ESP-IDF v5+ example
-#define BLINK_PERIOD 5000
-
-static led_strip_handle_t strip;
-static bool s_led_state = false;
+#define UPDATE_PERIOD_TICKS pdMS_TO_TICKS(50)  // 20 Hz
 
 void app_main(void) {
-  led_strip_config_t strip_cfg = {
-      .strip_gpio_num = 38,
-      .max_leds = 1,
-  };
-  led_strip_rmt_config_t rmt_cfg = {
-      .resolution_hz = 10 * 1000 * 1000,  // 10MHz
-      .flags.with_dma = false,
-  };
-  ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_cfg, &rmt_cfg, &strip));
+  platform_init_comms();
 
+  slam_system_t system;
+  slam_system_init(&system);
+
+  scan_t scan;
+  scan_init(&scan, 0, 0);
+
+  int start, elapsed;
   while (1) {
-    ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
+    start = xTaskGetTickCount();
 
-    char c = getchar();
+    INFO("Hello!");
 
-    ESP_LOGI(TAG, "Received character: %c", c);
+    // perceive
+    platform_get_lidar(&scan);
 
-    if (s_led_state) {
-      led_strip_set_pixel(strip, 0, 0, 255, 0);  // red
-      led_strip_refresh(strip);
-    } else {
-      led_strip_clear(strip);
-    }
-    s_led_state = !s_led_state;
-    vTaskDelay(pdMS_TO_TICKS(BLINK_PERIOD));
+    // process
+    // slam_system_process(&system, &scan);
+
+    elapsed = xTaskGetTickCount() - start;
+    vTaskDelay(UPDATE_PERIOD_TICKS - elapsed);
   }
 }
