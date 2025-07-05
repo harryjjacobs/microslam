f0c1f4404867df81e395dbbf1dc906812825603b
harryjjacobs <harryjjacobs@googlemail.com>
2025-07-05 14:03:26 +0100

    fix broken logging in slam library

--- slam/src/default_logging.c
+++ slam/src/default_logging.c
@@ -3,8 +3,12 @@
 #include "slam/logging.h"
 
 void logging_log(int level, const char *file, int line, const char *fmt, ...) {
+  char buf[256];
+
   va_list args;
   va_start(args, fmt);
-  log_log(level, file, line, fmt, args);
+  vsnprintf(buf, sizeof(buf), fmt, args);
   va_end(args);
+
+  log_log(level, file, line, "%s", buf);
 }
