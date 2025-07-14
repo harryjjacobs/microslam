9ae49aeb330261646af4e7cfd33cd61620286ded
harryjjacobs <harryjjacobs@googlemail.com>
2025-07-14 22:48:56 +0100

    add `scan_valid` function

--- slam/include/slam/scan.h
+++ slam/include/slam/scan.h
@@ -8,6 +8,7 @@
 #ifndef SLAM_SCAN_H_
 #define SLAM_SCAN_H_
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -18,5 +19,7 @@ void scan_reset(scan_t *scan);
 void scan_reset(scan_t *scan);
 
 void scan_add(scan_t *scan, uint16_t bearing, uint16_t range);
+
+bool scan_valid(const scan_t *scan, int index);
 
 #endif /* SLAM_SCAN_H_ */
--- slam/src/scan.c
+++ slam/src/scan.c
@@ -13,3 +13,7 @@ void scan_add(scan_t *scan, uint16_t bearing, uint16_t range) {
     scan->hits++;
   }
 }
+
+bool scan_valid(const scan_t *scan, int index) {
+  return scan->range[index] > 1e-6f;
+}
